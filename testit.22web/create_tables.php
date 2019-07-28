0eac2799cc2c531541a79a17e77bb97024851ea0
neurofire99 <neurofire99@gmail.com>
2019-07-28 16:23:32 +1000

    Added Children table to create_tables.php
    Also made email columns 100 chars long and password column 255 chars

--- testit.22web/create_tables.php
+++ testit.22web/create_tables.php
@@ -14,16 +14,31 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-// sql to create table
+// sql to create Users table
 $sql = "CREATE TABLE Users (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-email VARCHAR(50) NOT NULL,
-psswrd VARCHAR(50) NOT NULL,
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(100) UNIQUE NOT NULL,
+psswrd VARCHAR(255) NOT NULL,
 reg_date TIMESTAMP
 )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
+    echo "Table Users created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// sql to create Children table
+$sql = "CREATE TABLE Children (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+parent INT(6),
+email VARCHAR(100) NOT NULL,
+reg_date TIMESTAMP,
+FOREIGN KEY (parent) REFERENCES Users(id)
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Children created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
